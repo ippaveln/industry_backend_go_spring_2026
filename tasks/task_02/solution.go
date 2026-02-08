c228b337c5091c52020f108a6e6a56b73d0e6ffd
KomaMix <ksabirov448@gmail.com>
2026-02-08 20:34:35 +0300

    added solving third problem

--- tasks/task_02/solution.go
+++ tasks/task_02/solution.go
@@ -1,5 +1,14 @@
 package main
 
 func reverseRunes(s string) string {
-	// TODO
+	runes := []rune(s)
+
+	for i := 0; i < len(runes)/2; i++ {
+		var t rune
+		t = runes[i]
+		runes[i] = runes[len(runes)-1-i]
+		runes[len(runes)-1-i] = t
+	}
+
+	return string(runes)
 }
