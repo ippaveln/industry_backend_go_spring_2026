51097cb2d3494c5c410961dc6a6d37176f6eaa51
KomaMix <ksabirov448@gmail.com>
2026-02-14 11:06:33 +0300

    added solving ninth problem

--- tasks/task_08/solution.go
+++ tasks/task_08/solution.go
@@ -1,11 +1,53 @@
 package main
 
+import (
+	"sync"
+	"time"
+)
+
 type Clock interface {
-	// TODO
+	Now() time.Time
 }
 
 type Limiter struct {
-	// TODO
+	clock            Clock
+	lastMeasuredTime time.Time
+	mutex            sync.Mutex
+	ratePerSec       float64
+	tokens           float64
+	burst            int
 }
 
-// TODO
+func NewLimiter(clock Clock, ratePerSec float64, burst int) *Limiter {
+	return &Limiter{
+		clock:            clock,
+		lastMeasuredTime: clock.Now(),
+		ratePerSec:       ratePerSec,
+		tokens:           float64(burst),
+		burst:            burst,
+	}
+}
+
+func (l *Limiter) Allow() bool {
+	if l.burst <= 0 {
+		return false
+	}
+
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	curTime := l.clock.Now()
+	l.tokens += l.ratePerSec * curTime.Sub(l.lastMeasuredTime).Seconds()
+	if float64(l.burst) < l.tokens {
+		l.tokens = float64(l.burst)
+	}
+
+	l.lastMeasuredTime = curTime
+
+	if l.tokens >= 1 {
+		l.tokens -= 1
+		return true
+	}
+
+	return false
+}
