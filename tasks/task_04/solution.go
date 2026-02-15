b553820e1a0cc0b05df22faf08cb87c91c87318c
tnosenko <tnosenko@ozon.ru>
2026-02-15 23:27:40 +0300

    task 4

--- tasks/task_04/solution.go
+++ tasks/task_04/solution.go
@@ -1,9 +1,34 @@
 package main
 
 type Stats struct {
-	// TODO: define the struct
+	Count int
+	Sum   int64
+	Min   int64
+	Max   int64
 }
 
 func Calc(nums []int64) Stats {
-	// TODO
+    if len(nums) == 0 {
+        return Stats{}
+    }
+    
+    result := Stats{
+        Count: 1,
+        Sum:   nums[0],
+        Min:   nums[0],
+        Max:   nums[0],
+    }
+    
+    for i := 1; i < len(nums); i++ {
+        result.Count++
+        result.Sum += nums[i]
+        
+        if nums[i] < result.Min {
+            result.Min = nums[i]
+        }
+        if nums[i] > result.Max {
+            result.Max = nums[i]
+        }
+    }
+    return result
 }
