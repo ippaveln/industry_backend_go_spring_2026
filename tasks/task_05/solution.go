347ec11e50ce42d057d3db65ed45a8bde5f0399b
tnosenko <tnosenko@ozon.ru>
2026-02-18 01:07:33 +0300

    task 5

--- tasks/task_05/solution.go
+++ tasks/task_05/solution.go
@@ -1,3 +1,29 @@
 package main
+type Cache[K comparable, V any] struct {
+	data map[K]V
+	cap  int
+}
 
-// TODO
+func NewCache[K comparable, V any](cap int) *Cache[K, V] {
+	return &Cache[K, V]{
+		data: make(map[K]V, cap),
+		cap:  cap,
+	}
+}
+
+func (c *Cache[K, V]) Get(key K) (V, bool) {
+	if c.cap == 0 {
+		var zero V
+		return zero, false
+	}
+	value, ok := c.data[key]
+	if !ok {
+		var zero V
+		return zero, false
+	}
+	return value, true
+}
+
+func (c *Cache[K, V]) Set(key K, val V) {
+	c.data[key] = val
+}
