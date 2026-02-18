cbb4a3f9ce947d7eeb0566293e2676888b858916
Dmitriy Shevelev <dima.shevelev.42@gmail.com>
2026-02-18 13:45:12 +0300

    done task 5

--- tasks/task_05/solution.go
+++ tasks/task_05/solution.go
@@ -1,3 +1,25 @@
 package main
 
-// TODO
+type Cache[K comparable, V any] struct {
+	cache    map[K]V
+	capacity int
+}
+
+func NewCache[K comparable, V any](capacity int) *Cache[K, V] {
+	res := Cache[K, V]{}
+	res.cache = make(map[K]V, capacity)
+	res.capacity = capacity
+	return &res
+}
+
+func (c *Cache[K, V]) Set(key K, value V) {
+	if c.capacity == 0 {
+		return
+	}
+	c.cache[key] = value
+}
+
+func (c *Cache[K, V]) Get(key K) (V, bool) {
+	v, ok := c.cache[key]
+	return v, ok
+}
