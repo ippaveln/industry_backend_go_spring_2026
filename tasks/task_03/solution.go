93a5f900882ae66ea8d3586286a2880425ad96cc
Dmitriy Shevelev <dima.shevelev.42@gmail.com>
2026-02-18 12:45:26 +0300

    done task 3

--- tasks/task_03/solution.go
+++ tasks/task_03/solution.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func fizzBuzz(n int) (string, error) {
-	// TODO
+	if n < 0 {
+		return "", fmt.Errorf("Negative number!")
+	}
+	res := ""
+	if n%3 == 0 {
+		res += "Fizz"
+	}
+	if n%5 == 0 {
+		res += "Buzz"
+	}
+	if res == "" {
+		res = fmt.Sprintf("%d", n)
+	}
+	return res, nil
 }
