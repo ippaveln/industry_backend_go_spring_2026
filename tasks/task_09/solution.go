2960bee1e057bf5d499d8db1e50f45b7dd8645b9
Kseniia Astakhova <astakhova.ksyusha.07@mail.ru>
2026-02-25 18:48:12 +0500

    task_09

--- tasks/task_09/solution.go
+++ tasks/task_09/solution.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"sync"
 )
 
 func ParallelMap[T any, R any](
@@ -10,5 +11,57 @@ func ParallelMap[T any, R any](
 	in []T,
 	fn func(context.Context, T) (R, error),
 ) ([]R, error) {
-	// TODO
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithCancel(ctx)
+	if workers <= 0 {
+		cancel()
+		return nil, ctx.Err()
+	}
+	defer cancel()
+
+	var wg sync.WaitGroup
+	wg.Add(workers)
+	out := make([]R, len(in))
+	jobs := make(chan int)
+	var firstErr error
+	var once sync.Once
+
+	worker := func() {
+		defer wg.Done()
+		for j := range jobs {
+			if ctx.Err() != nil {
+				return
+			}
+			res, err := fn(ctx, in[j])
+			if err != nil {
+				once.Do(func() {
+					firstErr = err
+					cancel()
+				})
+			}
+			out[j] = res
+		}
+	}
+
+	for range workers {
+		go worker()
+	}
+
+	for i := range in {
+		select {
+		case <-ctx.Done():
+			break
+		case jobs <- i:
+		}
+	}
+	close(jobs)
+	wg.Wait()
+
+	if firstErr != nil {
+		return nil, firstErr
+	}
+
+	return out, nil
 }
