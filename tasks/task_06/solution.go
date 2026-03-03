17d7fbd2e23e7aa50a0d41eb8e556b35aa86e622
tnosenko <tnosenko@ozon.ru>
2026-03-03 23:58:15 +0300

    task 7

--- tasks/task_06/solution.go
+++ tasks/task_06/solution.go
@@ -1,119 +1,119 @@
 package main
 
 type LRUCache[K comparable, V any] struct {
-	data     map[K]*node[K, V]
-	capacity int
-	head     *node[K, V]
-	tail     *node[K, V]
+    data     map[K]*node[K, V]
+    capacity int
+    head     *node[K, V]
+    tail     *node[K, V]
 }
 
 type node[K comparable, V any] struct {
-	key   K
-	value V
-	prev  *node[K, V]
-	next  *node[K, V]
+    key   K
+    value V
+    prev  *node[K, V]
+    next  *node[K, V]
 }
 
 func NewLRUCache[K comparable, V any](capacity int) *LRUCache[K, V] {
-	return &LRUCache[K, V]{
-		data:     make(map[K]*node[K, V], capacity),
-		capacity: capacity,
-	}
+    return &LRUCache[K, V]{
+        data:     make(map[K]*node[K, V], capacity),
+        capacity: capacity,
+    }
 }
 
 func (c *LRUCache[K, V]) moveToFront(n *node[K, V]) {
-	if c.head == n {
-		return
-	}
+    if c.head == n {
+        return
+    }
 
-	if n.prev != nil {
-		n.prev.next = n.next
-	}
-	if n.next != nil {
-		n.next.prev = n.prev
-	}
-	if c.tail == n {
-		c.tail = n.prev
-	}
+    if n.prev != nil {
+        n.prev.next = n.next
+    }
+    if n.next != nil {
+        n.next.prev = n.prev
+    }
+    if c.tail == n {
+        c.tail = n.prev
+    }
 
-	n.prev = nil
-	n.next = c.head
-	if c.head != nil {
-		c.head.prev = n
-	}
-	c.head = n
+    n.prev = nil
+    n.next = c.head
+    if c.head != nil {
+        c.head.prev = n
+    }
+    c.head = n
 
-	if c.tail == nil {
-		c.tail = n
-	}
+    if c.tail == nil {
+        c.tail = n
+    }
 }
 
 func (c *LRUCache[K, V]) addToFront(n *node[K, V]) {
-	if c.head == nil {
-		c.head = n
-		c.tail = n
-		n.prev = nil
-		n.next = nil
-		return
-	}
+    if c.head == nil {
+        c.head = n
+        c.tail = n
+        n.prev = nil
+        n.next = nil
+        return
+    }
 
-	n.next = c.head
-	n.prev = nil
-	c.head.prev = n
-	c.head = n
+    n.next = c.head
+    n.prev = nil
+    c.head.prev = n
+    c.head = n
 }
 
 func (c *LRUCache[K, V]) removeOldest() {
-	if c.tail == nil {
-		return
-	}
+    if c.tail == nil {
+        return
+    }
 
-	oldestKey := c.tail.key
+    oldestKey := c.tail.key
 
-	c.tail = c.tail.prev
-	if c.tail != nil {
-		c.tail.next = nil
-	} else {
-		c.head = nil
-	}
+    c.tail = c.tail.prev
+    if c.tail != nil {
+        c.tail.next = nil
+    } else {
+        c.head = nil
+    }
 
-	delete(c.data, oldestKey)
+    delete(c.data, oldestKey)
 }
 
-func (C *LRUCache[K, V]) Get(key K) (V, bool) {
-	if C.capacity == 0 {
-		var zero V
-		return zero, false
-	}
-	node, ok := C.data[key]
-	if !ok {
-		var zero V
-		return zero, false
-	}
-	C.moveToFront(node)
-	return node.value, true
+func (c *LRUCache[K, V]) Get(key K) (V, bool) {
+    if c.capacity == 0 {
+        var zero V
+        return zero, false
+    }
+    node, ok := c.data[key]
+    if !ok {
+        var zero V
+        return zero, false
+    }
+    c.moveToFront(node)
+    return node.value, true
 }
 
-func (C *LRUCache[K, V]) Set(key K, value V) {
-	if C.capacity == 0 {
-		return
-	}
+func (c *LRUCache[K, V]) Set(key K, value V) {
+    if c.capacity == 0 {
+        return
+    }
 
-	if node, ok := C.data[key]; ok {
-		node.value = value
-		C.moveToFront(node)
-		return
-	}
+    if node, ok := c.data[key]; ok {
+        node.value = value
+        c.moveToFront(node)
+        return
+    }
 
-	if len(C.data) >= C.capacity {
-		C.removeOldest()
-	}
+    if len(c.data) >= c.capacity {
+        c.removeOldest()
+    }
 
-	newNode := &node[K, V]{
-		key:   key,
-		value: value,
-	}
+    newNode := &node[K, V]{
+        key:   key,
+        value: value,
+    }
 
-	C.data[key] = newNode
-	C.addToFront(newNode)
+    c.data[key] = newNode
+    c.addToFront(newNode)
 }
--- tasks/task_07/solution.go
+++ tasks/task_07/solution.go
@@ -1,8 +1,124 @@
 package main
 
+import (
+    "sync"
+)
+
 type LRU[K comparable, V any] interface {
-	Get(key K) (value V, ok bool)
-	Set(key K, value V)
+    Get(key K) (V, bool)
+    Set(key K, value V)
+}
+type node[K comparable, V any] struct {
+    key   K
+    value V
+    prev  *node[K, V]
+    next  *node[K, V]
+}
+type LRUCache[K comparable, V any] struct {
+    mu       sync.Mutex
+    capacity int
+    data     map[K]*node[K, V]
+    head     *node[K, V]
+    tail     *node[K, V]
 }
 
-// TODO
+func NewLRUCache[K comparable, V any](capacity int) *LRUCache[K, V] {
+    return &LRUCache[K, V]{
+        capacity: capacity,
+        data:     make(map[K]*node[K, V], capacity),
+    }
+}
+
+func (c *LRUCache[K, V]) Get(key K) (V, bool) {
+    c.mu.Lock()
+    defer c.mu.Unlock()
+
+    var zero V
+    if c.capacity == 0 {
+        return zero, false
+    }
+
+    if node, ok := c.data[key]; ok {
+        c.moveToFront(node)
+        return node.value, true
+    }
+    return zero, false
+}
+
+func (c *LRUCache[K, V]) Set(key K, value V) {
+    c.mu.Lock()
+    defer c.mu.Unlock()
+
+    if c.capacity == 0 {
+        return
+    }
+    if node, ok := c.data[key]; ok {
+        node.value = value
+        c.moveToFront(node)
+        return
+    }
+
+    if len(c.data) >= c.capacity {
+        c.removeLast()
+    }
+
+    newNode := &node[K, V]{
+        key:   key,
+        value: value,
+    }
+    c.data[key] = newNode
+    c.addToFront(newNode)
+}
+
+func (c *LRUCache[K, V]) moveToFront(n *node[K, V]) {
+    if n == c.head {
+        return
+    }
+
+    c.removeNode(n)
+    c.addToFront(n)
+}
+
+func (c *LRUCache[K, V]) addToFront(n *node[K, V]) {
+    n.prev = nil
+    n.next = c.head
+
+    if c.head != nil {
+        c.head.prev = n
+    }
+    c.head = n
+
+    if c.tail == nil {
+        c.tail = n
+    }
+}
+
+func (c *LRUCache[K, V]) removeNode(n *node[K, V]) {
+    if n.prev != nil {
+        n.prev.next = n.next
+    } else {
+        c.head = n.next
+    }
+
+    if n.next != nil {
+        n.next.prev = n.prev
+    } else {
+        c.tail = n.prev
+    }
+}
+
+func (c *LRUCache[K, V]) removeLast() {
+    if c.tail == nil {
+        return
+    }
+
+    delete(c.data, c.tail.key)
+
+    if c.tail.prev != nil {
+        c.tail.prev.next = nil
+        c.tail = c.tail.prev
+    } else {
+        c.head = nil
+        c.tail = nil
+    }
+}
