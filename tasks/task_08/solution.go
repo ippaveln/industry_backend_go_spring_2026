bfda1be15ba1e4fe9d5b41e9005bf5a07e55139c
ANANAzZzZz <vladscooltrol@gmail.com>
2026-03-07 15:42:08 +0300

    solve task_08

--- tasks/task_08/solution.go
+++ tasks/task_08/solution.go
@@ -1,11 +1,63 @@
 package main
 
+import (
+	"sync"
+	"time"
+)
+
+type TokenBucket interface {
+	Allow() (ok bool)
+}
+
 type Clock interface {
-	// TODO
+	Now() time.Time
 }
 
 type Limiter struct {
-	// TODO
+	currTokens float64
+	ratePerSec float64
+	burst      int
+	clock      Clock
+	mutex      sync.Mutex
+	lastRefill time.Time
 }
 
-// TODO
+func NewLimiter(clock Clock, ratePerSec float64, burst int) *Limiter {
+	limiter := &Limiter{
+		currTokens: float64(burst),
+		ratePerSec: ratePerSec,
+		burst:      burst,
+		clock:      clock,
+		lastRefill: clock.Now(),
+	}
+
+	return limiter
+}
+
+func (l *Limiter) Allow() (ok bool) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	l.RefillTokenBuket()
+
+	if l.burst < 1 || l.currTokens < 1 {
+		return false
+	}
+
+	l.currTokens--
+
+	return true
+}
+
+func (l *Limiter) RefillTokenBuket() {
+	elapsedTime := l.clock.Now().Sub(l.lastRefill)
+	rechargedTokens := elapsedTime.Seconds() * l.ratePerSec
+
+	l.lastRefill = l.clock.Now()
+
+	if rechargedTokens > float64(l.burst) {
+		l.currTokens = float64(l.burst)
+		return
+	}
+	l.currTokens += rechargedTokens
+}
