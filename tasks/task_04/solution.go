af275b418d09c7fa7500a48368b59730be026326
Katerina <katyatselovalnikova24@gmail.com>
2026-03-12 02:54:35 +0300

    task 04 done

--- tasks/task_04/solution.go
+++ tasks/task_04/solution.go
@@ -1,9 +1,32 @@
 package main
 
 type Stats struct {
-	// TODO: define the struct
+	Count int
+	Sum   int64
+	Min   int64
+	Max   int64
 }
 
 func Calc(nums []int64) Stats {
-	// TODO
+	if len(nums) == 0 {
+		return Stats{}
+	}
+
+	var s Stats
+	s.Count = len(nums)
+	s.Sum = 0
+	s.Min = nums[0]
+	s.Max = nums[0]
+
+	for _, v := range nums {
+		s.Sum += v
+		if v < s.Min {
+			s.Min = v
+		}
+		if v > s.Max {
+			s.Max = v
+		}
+	}
+
+	return s
 }
