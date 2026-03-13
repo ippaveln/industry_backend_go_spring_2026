34084c789b783c6382b5f93969bdbe91e709d544
bladway <shepliakov_vlad@mail.ru>
2026-03-13 22:44:36 +0300

    task_04 - dynamic and flowing stat forming

--- tasks/task_04/solution.go
+++ tasks/task_04/solution.go
@@ -1,9 +1,26 @@
 package main
 
+import "math"
+
 type Stats struct {
-	// TODO: define the struct
+	Count int
+	Sum   int64
+	Min   int64
+	Max   int64
 }
 
 func Calc(nums []int64) Stats {
-	// TODO
+	stat := Stats{}
+	if len(nums) == 0 {
+		return stat
+	}
+	stat.Min = math.MaxInt64
+	stat.Max = math.MinInt64
+	for _, n := range nums {
+		stat.Count++
+		stat.Sum += n
+		stat.Min = min(stat.Min, n)
+		stat.Max = max(stat.Max, n)
+	}
+	return stat
 }
