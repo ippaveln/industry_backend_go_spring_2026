e9a82c8e5e160551350382a9481ac04430ddec9c
bladway <shepliakov_vlad@mail.ru>
2026-03-14 02:26:27 +0300

    task_05 - simple cache using map realization with generics

--- tasks/task_05/solution.go
+++ tasks/task_05/solution.go
@@ -1,3 +1,28 @@
 package main
 
-// TODO
+type Cache[K comparable, V any] struct {
+	matching map[K]V
+}
+
+func NewCache[K comparable, V any](capacity int) *Cache[K, V] {
+	if capacity == 0 {
+		return &Cache[K, V]{}
+	}
+	return &Cache[K, V]{make(map[K]V, capacity)}
+}
+
+func (c *Cache[K, V]) Set(key K, value V) {
+	if c.matching == nil {
+		return
+	}
+	c.matching[key] = value
+}
+
+func (c *Cache[K, V]) Get(key K) (V, bool) {
+	if c.matching == nil {
+		var val V
+		return val, false
+	}
+	value, ok := c.matching[key]
+	return value, ok
+}
