36c19bd4a4f6fd699f9470ebb23ad30e03b5ac65
bladway <shepliakov_vlad@mail.ru>
2026-03-13 21:38:20 +0300

    task_02 - idiotic defer recursive string reversing

--- tasks/task_02/solution.go
+++ tasks/task_02/solution.go
@@ -1,5 +1,20 @@
 package main
 
+type stringBuilder string
+
+func (str *stringBuilder) addString(char rune) {
+	*str = *str + stringBuilder(string(char))
+}
+
+func (str *stringBuilder) reverse() {
+	for _, char := range *str {
+		defer str.addString(char)
+	}
+	*str = ""
+}
+
 func reverseRunes(s string) string {
-	// TODO
+	str := stringBuilder(s)
+	str.reverse()
+	return string(str)
 }
