e521d6917ee296a8ecda6f92a82fce09a13783e7
bladway <shepliakov_vlad@mail.ru>
2026-03-13 22:22:47 +0300

    task_03 - FizzBuzz filter chain realization

--- tasks/task_03/solution.go
+++ tasks/task_03/solution.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+)
+
 func fizzBuzz(n int) (string, error) {
-	// TODO
+	resultString := ""
+	if n < 0 {
+		return resultString, fmt.Errorf("negative number")
+	}
+	if n%3 == 0 {
+		resultString += "Fizz"
+	}
+	if n%5 == 0 {
+		resultString += "Buzz"
+	}
+	if resultString == "" {
+		return fmt.Sprintf("%d", n), nil
+	}
+	return resultString, nil
 }
