86e688b42d89eeafcd89a91c3c1dd2a1457f1175
Khrenov Alexander <AlexAndrKh.0305@gmail.com>
2026-03-28 11:31:24 +0300

    task_03 is done

--- tasks/task_03/solution.go
+++ tasks/task_03/solution.go
@@ -1,5 +1,39 @@
 package main
 
-func fizzBuzz(n int) (string, error) {
-	// TODO
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
+const (
+	numFizz = 3
+	numBuzz = 5
+
+	strFizz = "Fizz"
+	strBuzz = "Buzz"
+)
+
+var (
+	errNegativeNum = errors.New("passed number must not be negative")
+)
+
+func fizzBuzz(n int) (s string, err error) {
+	if n < 0 {
+		return "", errNegativeNum
+	}
+
+	var sb strings.Builder
+	sb.Grow(len(strFizz) + len(strBuzz))
+	if n%numFizz == 0 {
+		sb.WriteString(strFizz)
+	}
+	if n%numBuzz == 0 {
+		sb.WriteString(strBuzz)
+	}
+
+	if s = sb.String(); s == "" {
+		return strconv.Itoa(n), nil
+	}
+	return s, nil
 }
